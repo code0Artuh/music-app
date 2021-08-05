ff026769b2a3dfcf71527cc8e14a16aa0faad6d2
code0Artuh <arturrocha1.2@outlook.com>
2021-08-05 02:06:06 -0300

    main

--- templates/Static/js/data.js
+++ templates/Static/js/data.js
@@ -11,5 +11,29 @@ export default [
     artist: "Baker Ya Maker",
     cover: "66 Chambers.jpg",
     file: "66 Chambers.mp3"
-  }
+  },
+ {
+  title:"Fed up",
+  artist:"Ghostemane",
+  cover:"Fed Up.jpg",
+  file:"Fed Up.mp3"
+},
+{
+  title:"Darko",
+  artist:"Freddie Dredd",
+  cover:"Darko.gif",
+  file:"Darko.mp3"
+},
+{
+  title:"Im A Murderer",
+  artist:"Freddie Dredd",
+  cover:"Im A Murderer.jpg",
+  file:"Im A Murderer.mp3"
+},
+{
+  title:"Antartica",
+  artist:"SuicideBoys",
+  cover:"Antartica.jpg",
+  file:"Antartica.mp3"
+}
 ]
