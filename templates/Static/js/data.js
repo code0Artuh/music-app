ee665b73a3b5c094b2700ae38d987b4c4f58ac09
code0Artuh <arturrocha1.2@outlook.com>
2021-08-05 01:43:05 -0300

    main

---
+++ templates/Static/js/data.js
@@ -0,0 +1,15 @@
+export default [
+  {
+    title:
+      "Unapologetic",
+    artist: "Cloundead Records",
+    cover: "skeleton.gif",
+    file: "Unapologetic.mp3",
+  },
+  {
+    title: "66 Chambers",
+    artist: "Baker Ya Maker",
+    cover: "66 Chambers.jpg",
+    file: "66 Chambers.mp3"
+  }
+]
