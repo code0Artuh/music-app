71f4b8e4c4c6edb83bb251b02a0f54fdbeb5ab86
code0Artuh <arturrocha1.2@outlook.com>
2021-08-05 02:39:18 -0300

    main

--- templates/Static/js/data.js
+++ templates/Static/js/data.js
@@ -1,37 +1,37 @@
 export default [
   {
     title:
-      "Unapologetic",
+      "⛧Unapologetic⛧",
     artist: "Cloundead Records",
     cover: "skeleton.gif",
     file: "Unapologetic.mp3",
   },
   {
-    title: "66 Chambers",
+    title: "⛧66 Chambers⛧",
     artist: "Baker Ya Maker",
     cover: "66 Chambers.jpg",
     file: "66 Chambers.mp3"
   },
  {
-  title:"Fed up",
+  title:"⛧Fed up⛧",
   artist:"Ghostemane",
   cover:"Fed Up.jpg",
   file:"Fed Up.mp3"
 },
 {
-  title:"Darko",
+  title:"⛧Darko⛧",
   artist:"Freddie Dredd",
   cover:"Darko.gif",
   file:"Darko.mp3"
 },
 {
-  title:"Im A Murderer",
+  title:"⛧Im A Murderer⛧",
   artist:"Freddie Dredd",
   cover:"Im A Murderer.jpg",
   file:"Im A Murderer.mp3"
 },
 {
-  title:"Antartica",
+  title:"⛧Antartica⛧",
   artist:"SuicideBoys",
   cover:"Antartica.jpg",
   file:"Antartica.mp3"
