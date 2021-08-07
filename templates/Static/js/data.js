8a0e78ead466a356d31d9aa4e3586583ecb82f36
code0Artuh <arturrocha1.2@outlook.com>
2021-08-07 00:48:05 -0300

    main

--- templates/Static/js/data.js
+++ templates/Static/js/data.js
@@ -35,5 +35,230 @@ export default [
   artist:"SuicideBoys",
   cover:"Antartica.jpg",
   file:"Antartica.mp3"
-}
+},
+{
+  title:
+    "⛧2 Hot 4 U⛧",
+  artist: "SuicideBoys",
+  cover: "2 hot 4 u.jpg",
+  file: "2 hot 4 u.mp3",
+},
+{
+  title:
+    "⛧5 grand at 8 to 1⛧",
+  artist: "SuicideBoys",
+  cover: "5 grand.png",
+  file: "5 grand.mp3",
+},
+{
+  title:
+    "⛧Avalon⛧",
+  artist: "SuicideBoys",
+  cover: "avalon.jpeg",
+  file: "avalon.mp3",
+},
+{
+  title:
+    "⛧Bleach⛧",
+  artist: "SuicideBoys",
+  cover: "suicide.gif",
+  file: "bleach.mp3",
+},
+{
+  title:
+    "⛧Cult II⛧",
+  artist: "SuicideBoys",
+  cover: "suicide.gif",
+  file: "cult II.mp3",
+},
+{
+  title:
+    "⛧Degeneration in the key of a minor⛧",
+  artist: "SuicideBoys",
+  cover: "suicide.gif",
+  file: "degeneration.mp3",
+},
+{
+  title:
+    "⛧Doma Molchat⛧",
+  artist: "Molchat Doma",
+  cover: "skeleton.gif",
+  file: "doma molchat.mp3",
+},
+{
+  title:
+    "⛧Drop it like its hot!⛧",
+  artist: "Haarper",
+  cover: "skeleton.gif",
+  file: "drop it like.mp3",
+},
+{
+  title:
+    "⛧Eternal Flames⛧",
+  artist: "SuicideBoys",
+  cover: "suicide.gif",
+  file: "eternal flames.mp3",
+},
+{
+  title:
+    "⛧Forget it⛧",
+  artist: "SuicideBoys",
+  cover: "suicide.gif",
+  file: "forget it.mp3",
+},
+{
+  title:
+    "⛧...and to those i love,thanks for sticking around⛧",
+  artist: "SuicideBoys",
+  cover: "suicide.gif",
+  file: "frutas podres.mp3",
+},
+{
+  title:
+    "⛧Fuck Boy Blood Bath⛧",
+  artist: "SuicideBoys",
+  cover: "suicide.gif",
+  file: "fuck boy blood bath.mp3",
+},
+{
+  title:
+    "⛧Flesh⛧",
+  artist: "Ghostemane",
+  cover: "ghostemane.gif",
+  file: "ghostemane flesh.mp3",
+},
+{
+  title:
+    "⛧GTG⛧",
+  artist: "Freddie Dredd",
+  cover: "skeleton.gif",
+  file: "gtg.mp3",
+},
+{
+  title:
+    "⛧Guillotine⛧",
+  artist: "SuicideBoys",
+  cover: "suicide.gif",
+  file: "guillotine.mp3",
+},
+{
+  title:
+    "⛧Hotline⛧",
+  artist: "SuicideBoys",
+  cover: "suicide.gif",
+  file: "hotline.mp3",
+},
+{
+  title:
+    "⛧Lethargy⛧",
+  artist: "SuicideBoys",
+  cover: "suicide.gif",
+  file: "lethargy.mp3",
+},
+{
+  title:
+    "⛧lighting the flames of my own personal hell⛧",
+  artist: "SuicideBoys",
+  cover: "suicide.gif",
+  file: "lightining.mp3",
+},
+{
+  title:
+    "⛧Lights Out⛧",
+  artist: "Evilmane",
+  cover: "skeleton.gif",
+  file: "lights out.mp3",
+},
+{
+  title:
+    "⛧Mar⛧",
+  artist: "Ghostemane,Scarlxrd",
+  cover: "ghostemane.gif",
+  file: "mar.mp3",
+},
+{
+  title:
+    "⛧Materialism as a means to an end⛧",
+  artist: "SuicideBoys",
+  cover: "suicide.gif",
+  file: "materialism.mp3",
+},
+{
+  title:
+    "⛧Mount Sinai⛧",
+  artist: "SuicideBoys",
+  cover: "suicide.gif",
+  file: "mount sinai.mp3",
+},
+{
+  title:
+    "⛧New Profile Pic⛧",
+  artist: "SuicideBoys",
+  cover: "suicide.gif",
+  file: "new profile pic.mp3",
+},
+{
+  title:
+    "⛧Here We Go Again⛧",
+  artist: "SuicideBoys,Germ",
+  cover: "suicide.gif",
+  file: "suicideboys here we go.mp3",
+},
+{
+  title:
+    "⛧The number you have dialed is not in service⛧",
+  artist: "SuicideBoys",
+  cover: "suicide.gif",
+  file: "the number.mp3",
+},
+{
+  title:
+    "⛧The Witcher⛧",
+  artist: "SuicideBoys",
+  cover: "suicide.gif",
+  file: "the witcher.mp3",
+},
+{
+  title:
+    "⛧TripleSix⛧",
+  artist: "SuicideBoys",
+  cover: "suicide.gif",
+  file: "triplesix.mp3",
+},
+{
+  title:
+    "⛧Ugliest⛧",
+  artist: "SuicideBoys",
+  cover: "suicide.gif",
+  file: "ugliest.mp3",
+},
+{
+  title:
+    "⛧We Envy nothing in the world⛧",
+  artist: "SuicideBoys",
+  cover: "suicide.gif",
+  file: "we envy nothing in the world.mp3",
+},
+{
+  title:
+    "⛧WTH⛧",
+  artist: "Freddie Dredd",
+  cover: "skeleton.gif",
+  file: "wth.mp3",
+},
+{
+  title:
+    "⛧владыка⛧",
+  artist: "Ghostemane,Scarlxrd",
+  cover: "ghostemane.gif",
+  file: "владыка.mp3",
+},
+{
+  title:
+    "⛧навсегда⛧",
+  artist: "Ghostemane,Scarlxrd",
+  cover: "ghostemane.gif",
+  file: "навсегда.mp3",
+},
+
 ]
